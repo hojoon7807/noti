efba0b029c830d9a2b5fce307237c46c6a3ed386
hojoon <ujuj0202@gmail.com>
2022-11-02 20:56:18 +0900

    NOTI-14 수업 테스트 필드 추가

--- src/test/java/com/noti/noti/lesson/adapter/out/persistence/LessonPersistenceAdapterTest.java
+++ src/test/java/com/noti/noti/lesson/adapter/out/persistence/LessonPersistenceAdapterTest.java
@@ -2,7 +2,10 @@ package com.noti.noti.lesson.adapter.out.persistence;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.noti.noti.common.domain.model.Day;
 import com.noti.noti.lesson.domain.model.Lesson;
+import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
@@ -18,6 +21,7 @@ import org.springframework.test.context.ActiveProfiles;
 @Import({LessonPersistenceAdapter.class, LessonMapper.class})
 @DisplayName("LessonPersistenceAdapterTest 클래스")
 @DisplayNameGeneration(ReplaceUnderscores.class)
+@Slf4j
 class LessonPersistenceAdapterTest {
 
   @Autowired
@@ -32,12 +36,13 @@ class LessonPersistenceAdapterTest {
       final Lesson givenLesson =
           Lesson.builder()
               .lessonName("Lesson")
+              .days(Set.of(Day.MONDAY, Day.WEDNESDAY, Day.FRIDAY))
               .build();
 
       @Test
       void 주어진_객체를_저장하고_저장된_mapping된_객체를_반환한다() {
         Lesson savedLesson = lessonPersistenceAdapter.saveLesson(givenLesson);
-
+        log.info("days: {}",savedLesson.getDays());
         assertThat(savedLesson.getId()).isEqualTo(1L);
       }
     }
