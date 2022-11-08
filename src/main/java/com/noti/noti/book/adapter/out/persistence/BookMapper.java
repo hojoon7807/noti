1f1f40f7f6cd52034d8354d0e0136b50adf230ad
hojoon <ujuj0202@gmail.com>
2022-11-08 16:25:52 +0900

    NOTI-14 Book 도메인 구현

--- src/main/java/com/noti/noti/book/adapter/out/persistence/BookMapper.java
+++ src/main/java/com/noti/noti/book/adapter/out/persistence/BookMapper.java
@@ -2,12 +2,14 @@ package com.noti.noti.book.adapter.out.persistence;
 
 import com.noti.noti.book.adapter.out.persistence.jpa.model.BookJpaEntity;
 import com.noti.noti.book.domain.model.Book;
+import java.util.Calendar;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BookMapper {
 
   Book mapToDomainEntity(BookJpaEntity bookJpaEntity) {
+
     return Book.builder()
         .id(bookJpaEntity.getId())
         .title(bookJpaEntity.getTitle())
