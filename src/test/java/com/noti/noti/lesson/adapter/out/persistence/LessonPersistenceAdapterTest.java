f87560bf40bd798fc48aade35aa13c0fdb704861
hojoon <ujuj0202@gmail.com>
2022-12-30 23:20:38 +0900

    NOTI-28 [TEST] LesseonPersistenceAdapter 테스트 코드 작성

--- src/test/java/com/noti/noti/lesson/adapter/out/persistence/LessonPersistenceAdapterTest.java
+++ src/test/java/com/noti/noti/lesson/adapter/out/persistence/LessonPersistenceAdapterTest.java
@@ -2,10 +2,19 @@ package com.noti.noti.lesson.adapter.out.persistence;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.noti.noti.common.domain.model.Day;
+import com.noti.noti.common.adapter.out.persistance.DaySetConvertor;
+import com.noti.noti.config.QuerydslTestConfig;
+import com.noti.noti.lesson.adapter.out.persistence.jpa.LessonJpaRepository;
+import com.noti.noti.lesson.application.port.out.TodaysLesson;
+import com.noti.noti.lesson.application.port.out.TodaysLessonSearchConditon;
 import com.noti.noti.lesson.domain.model.Lesson;
+import com.noti.noti.teacher.adpater.out.persistence.TeacherMapper;
+import com.noti.noti.teacher.domain.Teacher;
+import java.time.DayOfWeek;
+import java.util.List;
 import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
@@ -15,10 +24,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest
 @ActiveProfiles("test")
-@Import({LessonPersistenceAdapter.class, LessonMapper.class})
+@Import({LessonPersistenceAdapter.class, LessonMapper.class, TeacherMapper.class,
+    DaySetConvertor.class, LessonQueryRepository.class, QuerydslTestConfig.class})
 @DisplayName("LessonPersistenceAdapterTest 클래스")
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @Slf4j
@@ -27,23 +38,66 @@ class LessonPersistenceAdapterTest {
   @Autowired
   LessonPersistenceAdapter lessonPersistenceAdapter;
 
+  @Autowired
+  LessonJpaRepository lessonJpaRepository;
+
+
   @Nested
   class save_메소드는 {
 
     @Nested
     class Lesson_도메인_객체가_주어지면 {
 
+      final Teacher giventeacher =
+          Teacher.builder()
+              .id(1L)
+              .build();
       final Lesson givenLesson =
           Lesson.builder()
               .lessonName("Lesson")
-              .days(Set.of(Day.MONDAY, Day.WEDNESDAY, Day.FRIDAY))
+              .days(Set.of(DayOfWeek.MONDAY, DayOfWeek.WEDNESDAY, DayOfWeek.FRIDAY))
+              .teacher(giventeacher)
               .build();
 
+      @Sql("/data/teacher.sql")
       @Test
       void 주어진_객체를_저장하고_저장된_mapping된_객체를_반환한다() {
         Lesson savedLesson = lessonPersistenceAdapter.saveLesson(givenLesson);
-        log.info("days: {}",savedLesson.getDays());
-        assertThat(savedLesson.getId()).isEqualTo(1L);
+        List<TodaysLesson> todaysLessons = lessonPersistenceAdapter.findTodaysLessons(
+            new TodaysLessonSearchConditon(1L));
+
+        assertThat(savedLesson.getLessonName()).isEqualTo(givenLesson.getLessonName());
+      }
+    }
+  }
+
+  @Sql("/data/lesson.sql")
+  @Nested
+  class findTodaysLessons_메소드는 {
+
+    @Nested
+    class 수업이_없는_선생님의_ID가_주어지면 {
+
+      final TodaysLessonSearchConditon condition = new TodaysLessonSearchConditon(3L);
+
+      @Test
+      void 비어있는_List를_반환한다() {
+        List<TodaysLesson> todaysLessons = lessonPersistenceAdapter.findTodaysLessons(condition);
+
+        assertThat(todaysLessons).isEmpty();
+      }
+    }
+
+    @Nested
+    class 수업이_있는_선생님의_ID가_주어지면 {
+      final TodaysLessonSearchConditon condition = new TodaysLessonSearchConditon(1L);
+
+      @Test
+      void 선생님_ID에_해당하는_수업목록_List를_반환한다(){
+        List<TodaysLesson> todaysLessons = lessonPersistenceAdapter.findTodaysLessons(condition);
+
+        System.out.println(todaysLessons.size());
+        assertThat(todaysLessons).isNotEmpty();
       }
     }
   }
