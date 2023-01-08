a98ea7bd4765447074f1b81518df5c3a7f0120cf
hojoon <ujuj0202@gmail.com>
2023-01-08 23:47:09 +0900

    NOTI-28 [FEAT] 선생님 ID에 해당하는 수업목록을 조회하기 위한 querydsl 로직과 apdater 구현
    선생님 ID에 해당하는 수업목록과 수업에 해당하는 학생목록의 결과를 수업 시작시간과, 학생 이름 순으로 조회한다

--- src/main/java/com/noti/noti/lesson/adapter/out/persistence/LessonPersistenceAdapter.java
+++ src/main/java/com/noti/noti/lesson/adapter/out/persistence/LessonPersistenceAdapter.java
@@ -2,22 +2,32 @@ package com.noti.noti.lesson.adapter.out.persistence;
 
 import com.noti.noti.lesson.adapter.out.persistence.jpa.LessonJpaRepository;
 import com.noti.noti.lesson.adapter.out.persistence.jpa.model.LessonJpaEntity;
+import com.noti.noti.lesson.application.port.out.FindTodaysLessonPort;
 import com.noti.noti.lesson.application.port.out.SaveLessonPort;
+import com.noti.noti.lesson.application.port.out.TodaysLesson;
+import com.noti.noti.lesson.application.port.out.TodaysLessonSearchConditon;
 import com.noti.noti.lesson.domain.model.Lesson;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Component
 @RequiredArgsConstructor
-public class LessonPersistenceAdapter implements SaveLessonPort {
+public class LessonPersistenceAdapter implements SaveLessonPort, FindTodaysLessonPort {
 
   private final LessonJpaRepository lessonJpaRepository;
   private final LessonMapper lessonMapper;
+  private final LessonQueryRepository lessonQueryRepository;
 
   @Override
   public Lesson saveLesson(Lesson lesson) {
     LessonJpaEntity lessonJpaEntity = lessonJpaRepository.save(lessonMapper.mapToJpaEntity(lesson));
     return lessonMapper.mapToDomainEntity(lessonJpaEntity);
+  }
+
+  @Override
+  public List<TodaysLesson> findTodaysLessons(TodaysLessonSearchConditon condition) {
+    return lessonQueryRepository.findTodayLesson(condition);
   }
 }
