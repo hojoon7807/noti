6e499c71db205f35e9ebe9ad3aa66f9872424d63
eunhee <dmsgml9903@gmail.com>
2023-01-12 16:37:20 +0900

    [FEAT] 날짜별 분반 수 조회 Controller

--- src/main/java/com/noti/noti/lesson/adapter/in/web/controller/GetFrequencyOfLessonsController.java
+++ src/main/java/com/noti/noti/lesson/adapter/in/web/controller/GetFrequencyOfLessonsController.java
@@ -3,21 +3,29 @@ package com.noti.noti.lesson.adapter.in.web.controller;
 import com.noti.noti.homework.adapter.in.web.dto.FrequencyOfLessonsDto;
 import com.noti.noti.homework.adapter.out.persistence.HomeworkPersistenceAdapter;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequiredArgsConstructor
 public class GetFrequencyOfLessonsController {
 
-  private HomeworkPersistenceAdapter homeworkPersistenceAdapter;
+  private final HomeworkPersistenceAdapter homeworkPersistenceAdapter;
 
-  @GetMapping("/api/teacher/calendar")
-  public ResponseEntity<List<FrequencyOfLessonsDto>> getFrequencyOfLessons(String monthYear) {
-    List<FrequencyOfLessonsDto> frequencyOfLessons = homeworkPersistenceAdapter.findFrequencyOfLessons(monthYear);
-    return ResponseEntity.ok(frequencyOfLessons);
+  @GetMapping("/api/teacher/calendar/{year}/{month}")
+  public ResponseEntity getFrequencyOfLessons(
+      @PathVariable String year,
+      @PathVariable String month) {
+
+    System.out.println(homeworkPersistenceAdapter.toString());
+    String yearMonth = new StringBuilder().append(year).append("-").append(month).toString();
+    List<FrequencyOfLessonsDto> responseDto = homeworkPersistenceAdapter.findFrequencyOfLessons(yearMonth);
+
+    return ResponseEntity.ok(responseDto);
   }
-
-
-
 
 
 }
