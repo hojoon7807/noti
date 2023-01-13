e8b7f18afae512742820d661473c343db48786e3
hojoon <ujuj0202@gmail.com>
2023-01-13 18:33:13 +0900

    NOTI-39 [FIX] OAuthUtil throws exception 과 빈 가져오는 방식 수정
    SocialType에 정의된 util 이름으로 해당 빈을 가져온다.

--- src/main/java/com/noti/noti/config/security/jwt/filter/KakaoOAuthUtil.java
+++ src/main/java/com/noti/noti/config/security/jwt/filter/KakaoOAuthUtil.java
@@ -1,27 +1,29 @@
 package com.noti.noti.config.security.jwt.filter;
 
 import com.noti.noti.error.exception.InvalidValueException;
+import com.noti.noti.error.exception.OauthAuthenticationException;
 import com.noti.noti.teacher.adpater.in.web.dto.KakaoDto.TeacherInfo;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-@Component("kakao")
+@Component
 public class KakaoOAuthUtil implements OAuthUtil {
 
   @Override
-  public String getSocialIdBy(String token) {
+  public String getSocialIdBy(String token) throws OauthAuthenticationException{
     TeacherInfo teacherInfo = WebClient.create("https://kapi.kakao.com/v2/user/me")
         .get()
         .header("Authorization", "Bearer " + token)
         .retrieve()
         .onStatus(HttpStatus::is4xxClientError, clientResponse -> clientResponse.bodyToMono(String.class)
             .flatMap(error ->
-                Mono.error(new InvalidValueException(error))))
+                Mono.error(new OauthAuthenticationException(error))))
         .onStatus(HttpStatus::is5xxServerError, clientResponse -> Mono.error(RuntimeException::new))
         .bodyToMono(TeacherInfo.class)
         .block();
+
 
     return teacherInfo.getId().toString();
   }
--- src/main/java/com/noti/noti/config/security/jwt/filter/OAuthManager.java
+++ src/main/java/com/noti/noti/config/security/jwt/filter/OAuthManager.java
@@ -1,5 +1,6 @@
 package com.noti.noti.config.security.jwt.filter;
 
+import com.noti.noti.error.exception.OauthAuthenticationException;
 import com.noti.noti.teacher.domain.SocialType;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +12,7 @@ public class OAuthManager {
 
   private final Map<String, OAuthUtil> authMap;
 
-  public String getSocialId(SocialType socialType, String token) {
-    return authMap.get(socialType.getSocialName()).getSocialIdBy(token);
+  public String getSocialId(SocialType socialType, String token) throws OauthAuthenticationException {
+      return authMap.get(socialType.getUtilName()).getSocialIdBy(token);
   }
 }
