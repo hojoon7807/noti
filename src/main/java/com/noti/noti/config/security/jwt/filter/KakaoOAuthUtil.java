db33d5a6fdca94994999698942fcd0a8a3281610
hojoon <ujuj0202@gmail.com>
2023-01-19 16:57:36 +0900

    NOTI-41 [FIX] KakaoOAuthUtil 리턴 타입 수정과 응답결과를 매핑할 클래스 구현

--- src/main/java/com/noti/noti/config/security/jwt/filter/KakaoOAuthUtil.java
+++ src/main/java/com/noti/noti/config/security/jwt/filter/KakaoOAuthUtil.java
@@ -1,5 +1,6 @@
 package com.noti.noti.config.security.jwt.filter;
 
+import com.noti.noti.aop.ExecutionTimeChecker;
 import com.noti.noti.error.exception.OauthAuthenticationException;
 import com.noti.noti.teacher.adpater.in.web.dto.KakaoOAuthInfo;
 import com.noti.noti.teacher.adpater.in.web.dto.OAuthInfo;
@@ -9,24 +10,28 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Component
+@ExecutionTimeChecker
 public class KakaoOAuthUtil implements OAuthUtil {
 
   @Override
-  public OAuthInfo getOAuthInfo(String token) throws OauthAuthenticationException{
+  public OAuthInfo getOAuthInfo(String token) throws OauthAuthenticationException {
     KakaoOAuthInfo kakaoOAuthInfo = WebClient.create("https://kapi.kakao.com/v2/user/me")
         .get()
         .header("Authorization", "Bearer " + token)
         .retrieve()
-        .onStatus(HttpStatus::is4xxClientError, clientResponse -> clientResponse.bodyToMono(String.class)
-            .flatMap(error ->
-                Mono.error(new OauthAuthenticationException(error))))
+        .onStatus(HttpStatus::is4xxClientError,
+            clientResponse -> clientResponse.bodyToMono(String.class)
+                .flatMap(error ->
+                    Mono.error(new OauthAuthenticationException(error))))
         .onStatus(HttpStatus::is5xxServerError, clientResponse -> Mono.error(RuntimeException::new))
         .bodyToMono(KakaoOAuthInfo.class)
         .block();
 
-    System.out.println(kakaoOAuthInfo);
-    return OAuthInfo.builder().socialId(kakaoOAuthInfo.getId())
+    return OAuthInfo.builder()
+        .socialId(kakaoOAuthInfo.getId())
         .nickname(kakaoOAuthInfo.getKakaoAccount().getProfile().getNickname())
-        .thumbnailImageUrl(kakaoOAuthInfo.getKakaoAccount().getProfile().getThumbnailImageUrl()).build();
+        .email(kakaoOAuthInfo.getKakaoAccount().getEmail())
+        .thumbnailImageUrl(kakaoOAuthInfo.getKakaoAccount().getProfile().getThumbnailImageUrl())
+        .build();
   }
 }
--- src/main/java/com/noti/noti/teacher/adpater/in/web/dto/OAuthInfo.java
+++ src/main/java/com/noti/noti/teacher/adpater/in/web/dto/OAuthInfo.java
@@ -8,12 +8,15 @@ public class OAuthInfo {
 
   private String socialId;
   private String nickname;
+
+  private String email;
   private String thumbnailImageUrl;
 
   @Builder
-  private OAuthInfo(String socialId, String nickname, String thumbnailImageUrl) {
+  private OAuthInfo(String socialId, String nickname, String thumbnailImageUrl, String email) {
     this.socialId = socialId;
     this.nickname = nickname;
+    this.email = email;
     this.thumbnailImageUrl = thumbnailImageUrl;
   }
 }
