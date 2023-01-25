85dd499224b844dd69690a15bafdee1092716e66
eunhee <dmsgml9903@gmail.com>
2023-01-25 19:52:54 +0900

    [TEST] FrequencyOfLessonsController 테스트 코드 작성

--- src/test/java/com/noti/noti/lesson/adapter/in/web/controller/GetFrequencyOfLessonsControllerTest.java
+++ src/test/java/com/noti/noti/lesson/adapter/in/web/controller/GetFrequencyOfLessonsControllerTest.java
@@ -1,126 +1,142 @@
 package com.noti.noti.lesson.adapter.in.web.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
+import com.noti.noti.common.WithAuthUser;
 import com.noti.noti.config.JacksonConfiguration;
 import com.noti.noti.config.security.jwt.JwtTokenProvider;
-import com.noti.noti.homework.adapter.in.web.dto.FrequencyOfLessonsDto;
-import com.noti.noti.homework.adapter.out.persistence.HomeworkPersistenceAdapter;
+import com.noti.noti.homework.domain.model.Homework;
+import com.noti.noti.lesson.adapter.in.web.dto.FrequencyOfLessonsDto;
+import com.noti.noti.lesson.application.port.in.GetFrequencyOfLessonsQuery;
 import com.noti.noti.lesson.application.port.in.GetTodaysLessonQuery;
+import com.noti.noti.lesson.domain.model.Lesson;
+import com.noti.noti.teacher.domain.Role;
+import com.noti.noti.teacher.domain.SocialType;
+import com.noti.noti.teacher.domain.Teacher;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
-import org.junit.Before;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
 @DisplayName("GetFrequencyOfLessonsControllerTest 클래스")
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @Import(JacksonConfiguration.class)
-@ExtendWith(SpringExtension.class)
 @WebMvcTest(GetFrequencyOfLessonsController.class)
-@ContextConfiguration(classes = GetFrequencyOfLessonsController.class)
 class GetFrequencyOfLessonsControllerTest {
 
-
-  private MockMvc mockMvc;
-
   @Autowired
-  private WebApplicationContext wac;
+  private MockMvc mockMvc;
 
   @MockBean
   private JwtTokenProvider jwtTokenProvider;
 
   @MockBean
-  private GetTodaysLessonQuery getTodaysLessonQuery;
-
-  @Autowired
-  private HomeworkPersistenceAdapter homeworkPersistenceAdapter;
-
-  private GetFrequencyOfLessonsController getFrequencyOfLessonsController;
+  private GetFrequencyOfLessonsQuery getFrequencyOfLessonsQuery;
 
 
+  List<FrequencyOfLessonsDto> createLessons() {
+
+    FrequencyOfLessonsDto dto1 = new FrequencyOfLessonsDto(LocalDateTime.now().plusDays(1), 2L);
+    FrequencyOfLessonsDto dto2 = new FrequencyOfLessonsDto(LocalDateTime.now().plusDays(2), 3L);
+    FrequencyOfLessonsDto dto3 = new FrequencyOfLessonsDto(LocalDateTime.now().plusDays(3), 4L);
+    FrequencyOfLessonsDto dto4 = new FrequencyOfLessonsDto(LocalDateTime.now().plusDays(4), 5L);
+
+    return List.of(dto1, dto2, dto3, dto4);
+  }
+
+  List<FrequencyOfLessonsDto> notCreateLessons() {
+    return List.of();
+  }
 
 
-  // 년-월 형식으로 제대로 줄 때
-      // 제대로 분반-개수 반환
-      // 빈 리스트 반환
-      // 서버 에러
-  // 형식이 맞지 않을 때 년-, 년-월-일
-      // 클라이언트 쪽 예외 발생
 
-  // 함수 - get 방식임 그 외에는 예외처리(클라이언트 문제)
-
-  /*
-  @Test
-    public void testGetProduct() throws Exception {
-        mockMvc.perform(get("/products/1")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(handler().handlerType(ProductController.class))
-                .andExpect(handler().methodName("getProduct"))
-                .andExpect(jsonPath("$.productId", is(1)))
-                .andExpect(jsonPath("$.productName", is("과자")))
-                .andExpect(jsonPath("$.price", is(105.5)))
-                .andDo(MockMvcResultHandlers.print());
-    }
-   */
   @Nested
   class 날짜_형식을_제대로_전달할_때 {
 
-    //@GetMapping("/api/teacher/calendar/{year}/{month}")
-
-    String yearMonth = "2023-1";
-    @Sql("/data/homework-lesson.sql")
     @Test
+    @WithAuthUser(id = "1", role = "TEACHER")
     void 주어진_날짜에_대한_날짜내용과_분반_수_반환() throws Exception {
+      when(getFrequencyOfLessonsQuery.findFrequencyOfLessons(any(String.class), any(Long.class)))
+          .thenReturn(createLessons());
 
-//      System.out.println(wac.getBean(GetFrequencyOfLessonsController.class));
-////      System.out.println(wac.getBean(HomeworkPersistenceAdapter.class));
-//
-//      /*mockMvc.perform(get("/api/teacher/calendar/2023/1")
-//              .accept(MediaType.APPLICATION_JSON))
-//          .andExpect(status().isOk())
-////          .andExpect(handler().handlerType(GetFrequencyOfLessonsController.class))
-//          .andExpect(handler().methodName("getFrequencyOfLessons"))
-//          .andDo(MockMvcResultHandlers.print());*/
+      LocalDate now = LocalDate.now();
+      String year = Integer.toString(now.getYear());
+      String month = Integer.toString(now.getMonth().getValue());
+      mockMvc.perform(MockMvcRequestBuilders.get("/api/teacher/calendar/{year}/{month}", year, month)) // ex. /api/teacher/calendar/2023/1
+          .andExpect(MockMvcResultMatchers.status().isOk())
+          .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(4));
+
+
     }
 
     @Test
-    void 빈_리스트_반환() {
+    @WithAuthUser(id = "1", role = "TEACHER")
+    void 빈_리스트_반환() throws Exception {
+      when(getFrequencyOfLessonsQuery.findFrequencyOfLessons(any(String.class), any(Long.class)))
+          .thenReturn(notCreateLessons());
+
+      LocalDate now = LocalDate.now();
+      String year = Integer.toString(now.getYear());
+      String month = Integer.toString(now.getMonth().getValue());
+
+      mockMvc.perform(MockMvcRequestBuilders.get("/api/teacher/calendar/{year}/{month}", year, month))
+          .andExpect(MockMvcResultMatchers.status().isOk())
+          .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(0));
 
     }
 
   }
 
   @Nested
-  @Sql("/data/homework-lesson.sql")
   class 날짜_형식을_제대로_전달하지_않았을_때 {
 
     @Test
-    void 예외코드_400발생() {
+    @WithAuthUser(id = "1", role = "TEACHER")
+    void 예외코드_400발생() throws Exception {
+      when(getFrequencyOfLessonsQuery.findFrequencyOfLessons(any(String.class), any(Long.class)))
+          .thenReturn(notCreateLessons());
+
+      LocalDate now = LocalDate.now();
+      String year = Integer.toString(now.getYear());
+      String month = now.getMonth().toString(); // 1이 아닌, JANUARY
+
+      mockMvc.perform(MockMvcRequestBuilders.get("/api/teacher/calendar/{year}/{month}", year, month))
+          .andExpect(MockMvcResultMatchers.status().is(400));
+
+    }
+
+  }
+
+  @Nested
+  class 선생님_id가_제대로_전달되지_않았을_때 {
+
+    @Test
+    void 예외코드_401발생() throws Exception {
+      when(getFrequencyOfLessonsQuery.findFrequencyOfLessons(any(String.class), any(Long.class)))
+          .thenReturn(createLessons());
+
+      LocalDate now = LocalDate.now();
+      String year = Integer.toString(now.getYear());
+      String month = Integer.toString(now.getMonth().getValue());
+
+      mockMvc.perform(MockMvcRequestBuilders.get("/api/teacher/calendar/{year}/{month}", year, month))
+          .andExpect(MockMvcResultMatchers.status().is(401));
 
     }
 
