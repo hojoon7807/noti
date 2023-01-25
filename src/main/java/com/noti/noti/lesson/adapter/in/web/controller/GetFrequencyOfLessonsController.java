98cf633685535ab5a88a45b473b5984eddaebe2f
eunhee <dmsgml9903@gmail.com>
2023-01-25 19:53:32 +0900

    [FIX] FrequencyOfLessonsController 오류 수정 - 년과 월을 String이 아닌 int로 받기

--- src/main/java/com/noti/noti/lesson/adapter/in/web/controller/GetFrequencyOfLessonsController.java
+++ src/main/java/com/noti/noti/lesson/adapter/in/web/controller/GetFrequencyOfLessonsController.java
@@ -2,7 +2,6 @@ package com.noti.noti.lesson.adapter.in.web.controller;
 
 import com.noti.noti.error.ErrorResponse;
 import com.noti.noti.lesson.adapter.in.web.dto.FrequencyOfLessonsDto;
-import com.noti.noti.homework.adapter.out.persistence.HomeworkPersistenceAdapter;
 import com.noti.noti.lesson.application.port.in.GetFrequencyOfLessonsQuery;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -25,7 +24,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class GetFrequencyOfLessonsController {
 
   private final GetFrequencyOfLessonsQuery getFrequencyOfLessonsQuery;
-  private final HomeworkPersistenceAdapter homeworkPersistenceAdapter;
 
 
   @Operation(summary = "FrequencyOfLessonsInfo", description = "요청 선생님의 월에 해당하는 수업 수와 날짜를 조회한다.")
@@ -43,7 +41,7 @@ public class GetFrequencyOfLessonsController {
   @Parameter(name = "userDetails", hidden = true)
   @GetMapping("/api/teacher/calendar/{year}/{month}")
   public ResponseEntity<List<FrequencyOfLessonsDto>> getFrequencyOfLessons(
-      @PathVariable String year, @PathVariable String month,
+      @PathVariable int year, @PathVariable int month,
       @AuthenticationPrincipal UserDetails userDetails) {
 
     long teacherId = Long.parseLong(userDetails.getUsername());
