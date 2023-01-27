d580b190e332de9df5faeda3f078f763906bd9cf
hojoon <ujuj0202@gmail.com>
2023-01-27 21:00:48 +0900

    NOTI-44 [FIX] Book 도메인 Teacher 와 연관관계 추가

--- src/main/java/com/noti/noti/book/adapter/out/persistence/BookMapper.java
+++ src/main/java/com/noti/noti/book/adapter/out/persistence/BookMapper.java
@@ -2,17 +2,23 @@ package com.noti.noti.book.adapter.out.persistence;
 
 import com.noti.noti.book.adapter.out.persistence.jpa.model.BookJpaEntity;
 import com.noti.noti.book.domain.model.Book;
+import com.noti.noti.teacher.adpater.out.persistence.TeacherMapper;
 import java.util.Calendar;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class BookMapper {
+
+  private final TeacherMapper teacherMapper;
 
   public Book mapToDomainEntity(BookJpaEntity bookJpaEntity) {
 
     return Book.builder()
         .id(bookJpaEntity.getId())
         .title(bookJpaEntity.getTitle())
+        .teacher(teacherMapper.mapToDomainEntity(bookJpaEntity.getTeacherJpaEntity()))
         .build();
   }
 
@@ -20,6 +26,7 @@ public class BookMapper {
     return BookJpaEntity.builder()
         .id(book.getId())
         .title(book.getTitle())
+        .teacherJpaEntity(teacherMapper.mapToJpaEntity(book.getTeacher()))
         .build();
   }
 }
