872ee431d02e4d203a84fd3186586478cd0d470c
eunhee <dmsgml9903@gmail.com>
2023-01-30 13:44:36 +0900

    [REFACT] 응답객체에 swagger 문서작성

--- src/main/java/com/noti/noti/lesson/adapter/in/web/dto/FrequencyOfLessonsDto.java
+++ src/main/java/com/noti/noti/lesson/adapter/in/web/dto/FrequencyOfLessonsDto.java
@@ -1,5 +1,6 @@
 package com.noti.noti.lesson.adapter.in.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import lombok.Getter;
@@ -9,7 +10,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class FrequencyOfLessonsDto {
 
+  @Schema(description = "숙업이 있는 날짜", example = "2023-01-27")
   private LocalDate dateOfLesson;
+  @Schema(description = "dateOfLesson 날짜에 해당하는 수업 수", example = "3")
   private Long frequencyOfLesson;
 
   public FrequencyOfLessonsDto(LocalDateTime dateOfLesson, Long frequencyOfLesson) {
--- src/main/java/com/noti/noti/studenthomework/adapter/in/web/dto/HomeworkOfGivenDateDto.java
+++ src/main/java/com/noti/noti/studenthomework/adapter/in/web/dto/HomeworkOfGivenDateDto.java
@@ -1,19 +1,26 @@
 package com.noti.noti.studenthomework.adapter.in.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
+@Getter
 @NoArgsConstructor
 public class HomeworkOfGivenDateDto {
 
+  @Schema(description = "수업 id", example = "1")
   public Long lessonId;
+  @Schema(description = "수업 이름", example = "중1 단어&독해")
   public String lessonName;
+  @Schema(description = "수업 시작 시간", example = "15:00")
   public LocalTime startTimeOfLesson;
+  @Schema(description = "수업 끝 시간", example = "17:00")
   public LocalTime endTimeOfLesson;
+  @Schema(description = "수업이 가지고 있는 숙제 리스트")
   public List<HomeworkDto> homeworks = new ArrayList<>();
 
   public HomeworkOfGivenDateDto(Long lessonId, String lessonName,
@@ -26,13 +33,17 @@ public class HomeworkOfGivenDateDto {
     this.homeworks = homeworks;
   }
 
-  @Data
+  @Getter
   @NoArgsConstructor
   public static class HomeworkDto {
 
+    @Schema(description = "숙제 id", example = "2")
     public Long homeworkId;
+    @Schema(description = "숙제 내용", example = "프린트물(기출문제) 풀기")
     public String homeworkContent;
+    @Schema(description = "숙제를 제공받은 학생 수", example = "7")
     public Long studentCnt;
+    @Schema(description = "숙제를 완료한 학생 수", example = "6")
     public Long completeCnt;
 
     public HomeworkDto(Long homeworkId, String homeworkContent, Long studentCnt,
